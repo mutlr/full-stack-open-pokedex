17e72cb3fc0cc1d04d1d9689a624fade93c28e39
mutlr <rmutlu01@hotmail.com>
2023-07-19 17:48:51 +0300

    Schedule

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('37') // change this string to ensure a new version deployed
+  res.send('39') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
+
   res.send('ok')
 })
 
