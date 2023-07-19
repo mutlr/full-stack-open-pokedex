de0ecada54885f3dff7898d815d24a31de7cba49
mutlr <rmutlu01@hotmail.com>
2023-07-19 17:36:27 +0300

    Schedule

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('35') // change this string to ensure a new version deployed
+  res.send('36') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
@@ -15,6 +15,6 @@ app.get('/health', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  / eslint-disable-next-line no-console
+  // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
